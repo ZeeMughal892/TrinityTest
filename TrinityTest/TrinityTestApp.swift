8c191b0a40533e06f2c06b482d82ec071d2d380f
Muhammad Zeeshan <zee.1mughal@gmail.com>
2022-07-16 09:37:32 -0700

    Initial Commit

---
+++ TrinityTest/TrinityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinityTestApp.swift
+//  TrinityTest
+//
+//  Created by Muhammad Zeeshan on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct TrinityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
